0570c866a847bc71ab5a10ac1969576ff14bcef3
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-11-16 09:24:56 -0500

    Fix issue with modifying pack-level rubocop todo files when moving files (#52)
    * Fix issue with manipulating pack level rubocop TODO files
    
    * Bump version

--- lib/use_packwerk/private/interactive_cli/use_cases/regenerate_rubocop_todo.rb
+++ lib/use_packwerk/private/interactive_cli/use_cases/regenerate_rubocop_todo.rb
@@ -11,13 +11,13 @@ module UsePackwerk
 
           sig { override.params(prompt: TTY::Prompt).void }
           def perform!(prompt)
-            packs = PackSelector.single_or_all_pack_multi_select(prompt, question_text: 'Please select the packs you want to regenerate `.rubocop_todo.yml` for')
+            packs = PackSelector.single_or_all_pack_multi_select(prompt, question_text: "Please select the packs you want to regenerate `#{RuboCop::Packs::PACK_LEVEL_RUBOCOP_TODO_YML}` for")
             RuboCop::Packs.auto_generate_rubocop_todo(packs: packs)
           end
 
           sig { override.returns(String) }
           def user_facing_name
-            'Regenerate packs/*/.rubocop_todo.yml for one or more packs'
+            "Regenerate packs/*/#{RuboCop::Packs::PACK_LEVEL_RUBOCOP_TODO_YML} for one or more packs"
           end
         end
       end
