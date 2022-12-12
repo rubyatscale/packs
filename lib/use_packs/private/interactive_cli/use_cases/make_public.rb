b6fc2808bcfd9f19dc92be02ff5bcc40c285a604
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-12-12 16:06:24 -0500

    Support simpler happy path for bin/packs move and make public

--- lib/use_packs/private/interactive_cli/use_cases/make_public.rb
+++ lib/use_packs/private/interactive_cli/use_cases/make_public.rb
@@ -16,11 +16,7 @@ module UsePacks
 
           sig { override.params(prompt: TTY::Prompt).void }
           def perform!(prompt)
-            raw_paths_relative_to_root = prompt.multiline('Please copy in a space or new line separated list of files or directories to make public')
-            paths_relative_to_root = T.let([], T::Array[String])
-            raw_paths_relative_to_root.each do |path|
-              paths_relative_to_root += path.chomp.split
-            end
+            paths_relative_to_root = FileSelector.select(prompt)
 
             UsePacks.make_public!(
               paths_relative_to_root: paths_relative_to_root,
