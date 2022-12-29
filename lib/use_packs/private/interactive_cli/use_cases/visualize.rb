7c168510a8f7d2bb0ad85b13ea99c0e2e5b02dce
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-12-29 12:50:17 -0500

    Change implementation to use packs

--- lib/use_packs/private/interactive_cli/use_cases/visualize.rb
+++ lib/use_packs/private/interactive_cli/use_cases/visualize.rb
@@ -22,14 +22,17 @@ module UsePacks
               by_name_or_by_owner = prompt.select('Do you select packs by name or by owner?', ['By name', 'By owner'])
               if by_name_or_by_owner == 'By owner'
                 teams = TeamSelector.multi_select(prompt)
-                selected_packs = ParsePackwerk.all.select do |p|
+                selected_packs = Packs.all.select do |p|
                   teams.map(&:name).include?(CodeOwnership.for_package(p)&.name)
                 end
               else
                 selected_packs = PackSelector.single_or_all_pack_multi_select(prompt)
               end
 
-              VisualizePackwerk.package_graph!(selected_packs)
+              packwerk_packages = selected_packs.map do |pack|
+                T.must(ParsePackwerk.find(pack.name))
+              end
+              VisualizePackwerk.package_graph!(packwerk_packages)
             end
           end
 
