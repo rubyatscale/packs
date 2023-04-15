069782ec4ec94e9128e741290c42cb04f35682e5
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-04-15 09:50:49 -0400

    give interactive CLI lint_package_todo_yml files capability

---
+++ lib/use_packs/private/interactive_cli/use_cases/lint_package_todo_yml_files.rb
@@ -0,0 +1,25 @@
+# typed: strict
+
+module UsePacks
+  module Private
+    module InteractiveCli
+      module UseCases
+        class LintPackageYmlTodoFiles
+          extend T::Sig
+          extend T::Helpers
+          include Interface
+
+          sig { override.params(prompt: TTY::Prompt).void }
+          def perform!(prompt)
+            Private.lint_package_todo_yml_files!
+          end
+
+          sig { override.returns(String) }
+          def user_facing_name
+            'Lint .package_todo.yml files'
+          end
+        end
+      end
+    end
+  end
+end
