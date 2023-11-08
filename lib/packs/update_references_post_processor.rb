5b8d34d9d2c7209e81454274ec1b881cd16afb9c
Perry Hertler <perry.hertler@gusto.com>
2023-11-08 17:21:43 -0600

    i_arg

--- lib/packs/update_references_post_processor.rb
+++ lib/packs/update_references_post_processor.rb
@@ -1,5 +1,6 @@
 # typed: strict
 
+require 'open3'
 module Packs
   class UpdateReferencesPostProcessor
     include PerFileProcessorInterface
@@ -19,9 +20,11 @@ module Packs
       destination_pack = T.must(file_move_operations.first).destination_pack.name
 
       if self.class.ripgrep_enabled?
-        p Dir.glob('./**/*')
-        `rg '#{origin_pack}' -l --hidden | xargs sed -i -e 's,#{origin_pack},#{destination_pack},g'`
-        p 'rg done'
+        cmd = "rg '#{origin_pack}' -l --hidden | xargs sed -i 's,#{origin_pack},#{destination_pack},g'"
+        puts cmd
+        puts "current dir: #{Dir.pwd}"
+        puts "current dir ls: #{Dir.children(Dir.pwd)}"
+        `#{cmd}`
       else
         Logging.print('For faster UpdateReferences install ripgrep: https://github.com/BurntSushi/ripgrep/tree/master')
         Dir.glob('./**/*', File::FNM_DOTMATCH) do |file_name|
@@ -37,6 +40,14 @@ module Packs
     sig { returns(T::Boolean) }
     def self.ripgrep_enabled?
       !!system('which', 'rg', out: File::NULL, err: :out)
+    end
+
+    private
+    def i_arg
+      _stdout_str, _stderr_str, status = Open3.capture3('sed', '--version')
+      puts "sed version: #{_stdout_str}"
+      puts "sed help: #{`sed --help`}`}"
+      status.success? ? "" : "''"
     end
   end
 end
