b0c9c213d0c883a3323b25135af11d05a3e1bd91
Teal ♥️  Stannard <teal.stannard@gusto.com>
2023-11-08 13:20:33 -0800

    rubocop

--- spec/packs_spec.rb
+++ spec/packs_spec.rb
@@ -1276,7 +1276,7 @@ RSpec.describe Packs do
     end
 
     describe 'UpdateReferencesPostProcessor' do
-      before do 
+      before do
         write_file('.some_other_file.yml')
 
         write_file('.some_other_file.yml', <<~CONTENTS)
@@ -1293,7 +1293,7 @@ RSpec.describe Packs do
         write_file('skims', <<~CONTENTS)
           kim kardashian
         CONTENTS
-      end 
+      end
 
       describe 'when riprep is installed' do
         before do
