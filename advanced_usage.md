446c694107edce59ab0e8ba03f4257cf7d49ac8f
Teal ♥️  Stannard <teal.stannard@gusto.com>
2023-11-08 11:47:08 -0800

    Update find and replace fallback to use ruby, update advanced usage
        Co-authored-by: Ashley Willard <ashley.willard@gusto.com>
        Co-authored-by: Perry Hertler <perry.hertler@gusto.com>
        Co-authored-by: Joey Schoblaska <joey.shoblaska@gusto.com>

--- advanced_usage.md
+++ advanced_usage.md
@@ -17,6 +17,8 @@ You can pass in an array of application specific behavior into the `per_file_pro
 
 See `rubocop_post_processor.rb` as an example of renaming files in `.rubocop_todo.yml` automatically, which is something you may want to do (as you do not want to fix all style errors when you're just moving a file).
 
+When moving packs you can update all references to the pack's path by using the UpdateReferencesPostProcessor. This uses [ripgrep](https://github.com/BurntSushi/ripgrep/tree/master) (if it is installed).
+
 # First-Time Configuration (per repo, not per developer)
 If you install binstubs, it allows simpler commands: `bin/packs` rather than `bundle exec packs`.
 
--- lib/packs/update_references_post_processor.rb
+++ lib/packs/update_references_post_processor.rb
@@ -14,16 +14,21 @@ module Packs
     sig { override.params(file_move_operations: T::Array[Private::FileMoveOperation]).void }
     def after_move_files!(file_move_operations)
       return if file_move_operations.empty?
-
-      unless self.class.ripgrep_enabled?
-        Logging.print('Skipping UpdateReferencesPostProcessor since ripgrep is not installed')
-        return
-      end
-
+ 
       origin_pack = T.must(file_move_operations.first).origin_pack.name
       destination_pack = T.must(file_move_operations.first).destination_pack.name
 
-      `rg '#{origin_pack}' -l --hidden | xargs sed -i '' 's,#{origin_pack},#{destination_pack},g'`
+      if self.class.ripgrep_enabled?
+        `rg '#{origin_pack}' -l --hidden | xargs sed -i '' 's,#{origin_pack},#{destination_pack},g'`
+      else
+        Logging.print('For faster UpdateReferences install ripgrep: https://github.com/BurntSushi/ripgrep/tree/master')
+        Dir.glob('./**/*', File::FNM_DOTMATCH) do |file_name|
+          next if File.directory?(file_name)
+          text = File.read(file_name)
+          replace = text.gsub(origin_pack, destination_pack)
+          File.open(file_name, 'w') { |file| file.puts replace }
+        end
+      end
     end
 
     sig { returns(T::Boolean) }
