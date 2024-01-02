fbfc8d97c7c2ee631d2bc2e019299ed5c5a8f22f
Joey Schoblaska <joey.schoblaska@gusto.com>
2024-01-01 18:02:48 -0600

    use += instead of <<

--- lib/packs/private/interactive_cli/pack_directory_selector.rb
+++ lib/packs/private/interactive_cli/pack_directory_selector.rb
@@ -8,15 +8,15 @@ module Packs
 
         sig { params(prompt: TTY::Prompt, question_text: String).returns(String) }
         def self.select(prompt, question_text: 'Select a directory')
-          directories = []
+          directories = T.let([], T::Array[String])
 
           Packs::Specification.config.pack_paths.each do |path|
-            directories << Dir.glob(path).select { |f| File.directory? f }
+            directories += Dir.glob(path).select { |f| File.directory? f }
           end
 
           prompt.select(
             question_text,
-            directories.flatten,
+            directories,
             filter: true,
             per_page: 10,
             show_help: :always
