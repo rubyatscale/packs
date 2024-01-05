b48cfc4255eda7a535df39b6154d1f02b9211905
Joey Schoblaska <joey.schoblaska@gusto.com>
2024-01-05 15:04:02 -0600

    Interactive CLI option for move_to_folder (#140)
    * first pass at creating a "move pack" interactive cli item
    
    Co-authored-by: Perry Hertler <perry.hertler@gusto.com>
    Co-authored-by: Ashley Willard <ashley.willard@gusto.com>
    Co-authored-by: Teal Stannard <teal.stannard@gusto.com>
    
    * specs for interactive cli changes
    
    * bump gem version
    
    * formatting
    
    * rubocop fix
    
    * flatten dirs and add spec for pack dir selector
    
    * use += instead of <<
    
    * use match_array to ignore ordering
    
    ---------
    
    Co-authored-by: Perry Hertler <perry.hertler@gusto.com>
    Co-authored-by: Ashley Willard <ashley.willard@gusto.com>
    Co-authored-by: Teal Stannard <teal.stannard@gusto.com>

---
+++ lib/packs/private/interactive_cli/use_cases/move_pack.rb
@@ -0,0 +1,70 @@
+# typed: strict
+
+module Packs
+  module Private
+    module InteractiveCli
+      module UseCases
+        class MovePack
+          extend T::Sig
+          extend T::Helpers
+          include Interface
+
+          sig { override.params(prompt: TTY::Prompt).void }
+          def perform!(prompt)
+            move_type = prompt.select(
+              'What do you want to do?',
+              {
+                'Move a child pack to be nested under a parent pack' => :move_to_parent,
+                'Move a pack to a folder that is not a pack' =>
+                 :move_to_folder
+              }
+            )
+
+            case move_type
+            when :move_to_parent
+              child_pack = PackSelector.single_pack_select(prompt, question_text: 'Please select the child pack that will be nested')
+              parent_pack = PackSelector.single_pack_select(prompt, question_text: 'Please select the pack that will be the parent')
+
+              Packs.move_to_parent!(
+                parent_name: parent_pack.name,
+                pack_name: child_pack.name,
+                per_file_processors: [Packs::RubocopPostProcessor.new, Packs::CodeOwnershipPostProcessor.new]
+              )
+            when :move_to_folder
+              pack = PackSelector.single_pack_select(prompt, question_text: 'Please select the pack that you want to move')
+              destination = PackDirectorySelector.select(prompt, question_text: "What directory do you want to move #{pack.name} to?")
+
+              if Packs.find(destination)
+                use_move_to_parent = prompt.select(
+                  "The directory #{destination} contains a pack. Add #{pack.last_name} as a dependency?",
+                  { 'Yes' => true, 'No' => false }
+                )
+
+                if use_move_to_parent
+                  Packs.move_to_parent!(
+                    parent_name: destination,
+                    pack_name: pack.name,
+                    per_file_processors: [Packs::RubocopPostProcessor.new, Packs::CodeOwnershipPostProcessor.new]
+                  )
+
+                  return
+                end
+              end
+
+              Packs.move_to_folder!(
+                pack_name: pack.name,
+                destination: destination,
+                per_file_processors: [Packs::RubocopPostProcessor.new, Packs::CodeOwnershipPostProcessor.new]
+              )
+            end
+          end
+
+          sig { override.returns(String) }
+          def user_facing_name
+            'Move a pack'
+          end
+        end
+      end
+    end
+  end
+end
--- packs.gemspec
+++ packs.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = 'packs'
-  spec.version       = '0.0.38'
+  spec.version       = '0.0.39'
   spec.authors       = ['Gusto Engineers']
   spec.email         = ['dev@gusto.com']
 
